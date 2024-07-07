83fe42c84285ba939c3a80df1cb952e37ee33fe8
Renal37 <renalplaycsgo@gmail.com>
2024-07-07 16:46:00 +0300

    Исправил go test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
-	"github.com/gorilla/mux"
+    "crypto/sha256"
+    "encoding/hex"
+    "fmt"
+    "io"
+    "net/http"
+    "github.com/gorilla/mux"
 	"github.com/Renal37/musthave_shortener_tpl.git/config"
-	"io"
-	"net/http"
-	"os"
 )
 
 // form - HTML-форма для ввода пользователем URL.
@@ -24,66 +23,58 @@ const form = `<html>
     </body>
 </html>`
 
-var cfg *config.Config
-
 // ShortenURL принимает URL в качестве входных данных и возвращает сокращенную версию.
 func ShortenURL(url string) string {
-	hasher := sha256.New()
-	hasher.Write([]byte(url))
-	return hex.EncodeToString(hasher.Sum(nil))[:8]
+    hasher := sha256.New()
+    hasher.Write([]byte(url))
+    return hex.EncodeToString(hasher.Sum(nil))[:8]
 }
 
 var originalURLs = map[string]string{
-	"EwHXdJfB": "https://example.com/original-url",
-	"1395ec37": "https://vk.com",
-	"3c0a9a5c": "https://practicum.yandex.ru/profile/go-advanced/",
+    "EwHXdJfB": "https://example.com/original-url",
 }
 
 // mainPage обрабатывает HTTP-запросы для главной страницы и нового эндпоинта.
 func mainPage(baseURL string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			url := r.FormValue("url")
-			shortenedURL := ShortenURL(url)
-			originalURLs[shortenedURL] = url // Сохранение оригинального URL в карте
-			w.WriteHeader(http.StatusCreated)
-			io.WriteString(w, fmt.Sprintf(`<p>Оригинальная URL: <a href="%s">%s</a></p>`, url, url))
-			io.WriteString(w, fmt.Sprintf(`<p>Сокращенная URL: <a href="%s/%s">%s/%s</a></p>`, baseURL, shortenedURL, baseURL, shortenedURL))
-			io.WriteString(w, form)
-		} else {
-			io.WriteString(w, form)
-		}
-	}
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.Method == http.MethodPost {
+            url := r.FormValue("url")
+            shortenedURL := ShortenURL(url)
+            originalURLs[shortenedURL] = url // Сохранение оригинального URL в карту
+            w.WriteHeader(http.StatusCreated)
+            io.WriteString(w, fmt.Sprintf(`<p>Original URL: <a href="%s">%s</a></p>`, url, url))
+            io.WriteString(w, fmt.Sprintf(`<p>Shortened URL: <a href="%s/%s">%s/%s</a></p>`, baseURL, shortenedURL, baseURL, shortenedURL))
+            io.WriteString(w, form)
+        } else {
+            io.WriteString(w, form)
+        }
+    }
 }
 
 // redirectHandler обрабатывает перенаправления с сокращенного URL на оригинальный URL.
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	shortenedURL := vars["id"]
-	originalURL, ok := originalURLs[shortenedURL]
-	if !ok {
-		http.Error(w, "Вы ввели не правильный URL", http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
+    vars := mux.Vars(r)
+    shortenedURL := vars["id"]
+    originalURL, ok := originalURLs[shortenedURL]
+    if !ok {
+        http.Error(w, "Invalid shortened URL", http.StatusBadRequest)
+        return
+    }
+    w.Header().Set("Location", originalURL)
+    w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
-// main-функция запускает HTTP-сервер и ожидает входящих запросов.
+// main-функция запускает HTTP-сервер с использованием конфигурации.
 func main() {
-	// Инициализация конфигурации
-	cfg = config.InitConfig()
+    cfg := config.InitConfig()
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", mainPage(cfg.BaseURL)).Methods(http.MethodGet, http.MethodPost)
-	r.HandleFunc("/{id}", redirectHandler).Methods(http.MethodGet)
+    r := mux.NewRouter()
+    r.HandleFunc("/", mainPage(cfg.BaseURL)).Methods(http.MethodGet, http.MethodPost)
+    r.HandleFunc("/{id}", redirectHandler).Methods(http.MethodGet)
 
-	http.Handle("/", r)
-
-	// Запуск HTTP-сервера
-	err := http.ListenAndServe(cfg.ServerAddress, nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка запуска HTTP-сервера: %v\n", err)
-		os.Exit(1)
-	}
+    http.Handle("/", r)
+    err := http.ListenAndServe(cfg.ServerAddress, nil)
+    if err != nil {
+        panic(err)
+    }
 }
