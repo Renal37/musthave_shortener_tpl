a244234e043813e2916a8241b69812e101ef4d17
Renal37 <renalplaycsgo@gmail.com>
2024-07-11 13:07:34 +0300

    Изменил на log

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-    "crypto/sha256"
-    "encoding/hex"
-    "fmt"
-    "io"
-    "net/http"
-    "github.com/gorilla/mux"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"github.com/Renal37/musthave_shortener_tpl.git/config"
+	"github.com/gorilla/mux"
 )
 
 // form - HTML-форма для ввода пользователем URL.
@@ -25,56 +27,56 @@ const form = `<html>
 
 // ShortenURL принимает URL в качестве входных данных и возвращает сокращенную версию.
 func ShortenURL(url string) string {
-    hasher := sha256.New()
-    hasher.Write([]byte(url))
-    return hex.EncodeToString(hasher.Sum(nil))[:8]
+	hasher := sha256.New()
+	hasher.Write([]byte(url))
+	return hex.EncodeToString(hasher.Sum(nil))[:8]
 }
 
 var originalURLs = map[string]string{
-    "EwHXdJfB": "https://example.com/original-url",
+	"EwHXdJfB": "https://example.com/original-url",
 }
 
 // mainPage обрабатывает HTTP-запросы для главной страницы и нового эндпоинта.
 func mainPage(baseURL string) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == http.MethodPost {
-            url := r.FormValue("url")
-            shortenedURL := ShortenURL(url)
-            originalURLs[shortenedURL] = url // Сохранение оригинального URL в карту
-            w.WriteHeader(http.StatusCreated)
-            io.WriteString(w, fmt.Sprintf(`<p>Original URL: <a href="%s">%s</a></p>`, url, url))
-            io.WriteString(w, fmt.Sprintf(`<p>Shortened URL: <a href="%s/%s">%s/%s</a></p>`, baseURL, shortenedURL, baseURL, shortenedURL))
-            io.WriteString(w, form)
-        } else {
-            io.WriteString(w, form)
-        }
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			url := r.FormValue("url")
+			shortenedURL := ShortenURL(url)
+			originalURLs[shortenedURL] = url // Сохранение оригинального URL в карту
+			w.WriteHeader(http.StatusCreated)
+			io.WriteString(w, fmt.Sprintf(`<p>Original URL: <a href="%s">%s</a></p>`, url, url))
+			io.WriteString(w, fmt.Sprintf(`<p>Shortened URL: <a href="%s/%s">%s/%s</a></p>`, baseURL, shortenedURL, baseURL, shortenedURL))
+			io.WriteString(w, form)
+		} else {
+			io.WriteString(w, form)
+		}
+	}
 }
 
 // redirectHandler обрабатывает перенаправления с сокращенного URL на оригинальный URL.
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    shortenedURL := vars["id"]
-    originalURL, ok := originalURLs[shortenedURL]
-    if !ok {
-        http.Error(w, "Invalid shortened URL", http.StatusBadRequest)
-        return
-    }
-    w.Header().Set("Location", originalURL)
-    w.WriteHeader(http.StatusTemporaryRedirect)
+	vars := mux.Vars(r)
+	shortenedURL := vars["id"]
+	originalURL, ok := originalURLs[shortenedURL]
+	if !ok {
+		http.Error(w, "Invalid shortened URL", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 // main-функция запускает HTTP-сервер с использованием конфигурации.
 func main() {
-    cfg := config.InitConfig()
+	cfg := config.InitConfig()
 
-    r := mux.NewRouter()
-    r.HandleFunc("/", mainPage(cfg.BaseURL)).Methods(http.MethodGet, http.MethodPost)
-    r.HandleFunc("/{id}", redirectHandler).Methods(http.MethodGet)
+	r := mux.NewRouter()
+	r.HandleFunc("/", mainPage(cfg.BaseURL)).Methods(http.MethodGet, http.MethodPost)
+	r.HandleFunc("/{id}", redirectHandler).Methods(http.MethodGet)
 
-    http.Handle("/", r)
-    err := http.ListenAndServe(cfg.ServerAddress, nil)
-    if err != nil {
-        panic(err)
-    }
+	http.Handle("/", r)
+	err := http.ListenAndServe(cfg.ServerAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
