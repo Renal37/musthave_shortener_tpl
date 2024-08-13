a376176da99102a8287c8708542dcb2c275a4a6d
Renal37 <renalplaycsgo@gmail.com>
2024-08-13 18:37:22 +0300

    Проверка инкремента

--- store/store.go
+++ store/store.go
@@ -12,66 +12,70 @@ type StoreDB struct {
 	db *sql.DB
 }
 
+// Инициализация базы данных
 func InitDatabase(DatabasePath string) (*StoreDB, error) {
-	db, err := sql.Open("pgx", DatabasePath)
+	db, err := sql.Open("pgx", DatabasePath) // Открытие соединения с базой данных
 	if err != nil {
-		return nil, fmt.Errorf("error opening db: %w", err)
+		return nil, fmt.Errorf("ошибка при открытии базы данных: %w", err)
 	}
 
-	storeDB := new(StoreDB)
-	storeDB.db = db
+	storeDB := &StoreDB{db: db}
 
 	if DatabasePath != "" {
-		err = createTable(db)
+		err = createTable(db) // Создание таблицы, если путь к базе данных не пустой
 		if err != nil {
-			return nil, fmt.Errorf("error creae table db: %w", err)
+			return nil, fmt.Errorf("ошибка при создании таблицы в базе данных: %w", err)
 		}
 	}
 
 	return storeDB, nil
 }
 
+// Создание записи в базе данных
 func (s *StoreDB) Create(originalURL, shortURL string) error {
 	query := `
         INSERT INTO urls (short_id, original_url) 
         VALUES ($1, $2)
     `
-	_, err := s.db.Exec(query, shortURL, originalURL)
+	_, err := s.db.Exec(query, shortURL, originalURL) // Вставка данных в таблицу
 	if err != nil {
-		return err
+		return fmt.Errorf("ошибка при создании записи: %w", err)
 	}
 	return nil
 }
 
+// Создание таблицы, если она не существует
 func createTable(db *sql.DB) error {
-	query := `CREATE TABLE IF NOT EXISTS urls (
-		id SERIAL PRIMARY KEY,
-		short_id VARCHAR(256) NOT NULL UNIQUE,
-		original_url TEXT NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);
-	DO $$ 
-	BEGIN 
-   	 IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE tablename = 'urls' AND indexname = 'idx_original_url') THEN
-        CREATE UNIQUE INDEX idx_original_url ON urls(original_url);
-    END IF;
-	END $$;`
+	query := `
+		CREATE TABLE IF NOT EXISTS urls (
+			id SERIAL PRIMARY KEY,
+			short_id VARCHAR(256) NOT NULL UNIQUE,
+			original_url TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+		DO $$ 
+		BEGIN 
+			IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE tablename = 'urls' AND indexname = 'idx_original_url') THEN
+				CREATE UNIQUE INDEX idx_original_url ON urls(original_url);
+			END IF;
+		END $$;
+	`
 
-	_, err := db.Exec(query)
+	_, err := db.Exec(query) // Выполнение SQL-запроса для создания таблицы
 	if err != nil {
-		return err
+		return fmt.Errorf("ошибка при создании таблицы: %w", err)
 	}
 
 	return nil
 }
 
-func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
+// Получение оригинального или сокращённого URL из базы данных
+func (s *StoreDB) Get(shortURL, originalURL string) (string, error) {
 	field1 := "original_url"
 	field2 := "short_id"
 	field := shortURL
 	if shortURL == "" {
-		field2 = "original_url"
-		field1 = "short_id"
+		field1, field2 = field2, field1
 		field = originalURL
 	}
 
@@ -81,23 +85,24 @@ func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
         WHERE %s = $1
     `, field1, field2)
 
-	var answer string
-	err := s.db.QueryRow(query, field).Scan(&answer)
+	var result string
+	err := s.db.QueryRow(query, field).Scan(&result) // Выполнение запроса и сканирование результата
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return "", err
+			return "", fmt.Errorf("запись не найдена: %w", err)
 		}
-		return "", err
+		return "", fmt.Errorf("ошибка при выполнении запроса: %w", err)
 	}
 
-	return answer, err
+	return result, nil
 }
 
+// Проверка соединения с базой данных
 func (s *StoreDB) PingStore() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	if err := s.db.PingContext(ctx); err != nil {
-		return fmt.Errorf("pinging db-store: %w", err)
+		return fmt.Errorf("ошибка при проверке соединения с базой данных: %w", err)
 	}
 	return nil
 }
