e31f11391d6cdd1f99904b829873309d6d1b48a3
Renal37 <renalplaycsgo@gmail.com>
2024-08-26 02:41:54 +0300

    Проверка автотеста

--- internal/dump/dump.go
+++ internal/dump/dump.go
@@ -4,86 +4,84 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/Renal37/musthave_shortener_tpl.git/internal/services"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
 	"io"
 	"os"
 	"strconv"
 )
 
+type Memory struct {
+	Storage *services.ShortenerService
+	File    *os.File
+}
 type ShortCollector struct {
 	NumberUUID  string `json:"uuid"`
 	ShortURL    string `json:"short_url"`
 	OriginalURL string `json:"original_url"`
 }
 
-// FillFromStorage загружает данные из файла в хранилище
 func FillFromStorage(storageInstance *storage.Storage, filePath string) error {
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	newDecoder := json.NewDecoder(file)
 	maxUUID := 0
-
 	for {
 		var event ShortCollector
 		if err := newDecoder.Decode(&event); err != nil {
 			if err == io.EOF {
 				break
 			} else {
-				fmt.Println("Ошибка при декодировании JSON:", err)
+				fmt.Println("error decode JSON:", err)
 				break
 			}
 		}
-		maxUUID++
+		maxUUID += 1
 		storageInstance.Set(event.OriginalURL, event.ShortURL)
 	}
 	return nil
 }
 
-// Set сохраняет данные из хранилища в файл
 func Set(storageInstance *storage.Storage, filePath string, BaseURL string) error {
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	maxUUID := 0
 	for shortURL, originalURL := range storageInstance.URLs {
-		maxUUID++
-		collector := ShortCollector{
-			NumberUUID:  strconv.Itoa(maxUUID),
-			ShortURL:    shortURL,
-			OriginalURL: originalURL,
+		//shortURL := fmt.Sprintf("%s/%s", BaseURL, key)
+		maxUUID += 1
+		ShortCollector := ShortCollector{
+			strconv.Itoa(maxUUID),
+			shortURL,
+			originalURL,
 		}
 		writer := bufio.NewWriter(file)
-		err = writeEvent(&collector, writer)
-		if err != nil {
-			return err
-		}
+		err = writeEvent(&ShortCollector, writer)
 	}
-	return nil
+	return err
 }
 
-func writeEvent(collector *ShortCollector, writer *bufio.Writer) error {
-	data, err := json.Marshal(collector)
+func writeEvent(ShortCollector *ShortCollector, writer *bufio.Writer) error {
+	data, err := json.Marshal(&ShortCollector)
 	if err != nil {
 		return err
 	}
 
-	// Записываем событие в буфер
+	// записываем событие в буфер
 	if _, err := writer.Write(data); err != nil {
 		return err
 	}
 
-	// Добавляем перенос строки
+	// добавляем перенос строки
 	if err := writer.WriteByte('\n'); err != nil {
 		return err
 	}
 
-	// Сбрасываем буфер в файл
+	// записываем буфер в файл
 	return writer.Flush()
 }
