7ef8b38f752f629cb0d16f304633931e1f45fcfa
Renal37 <renalplaycsgo@gmail.com>
2024-08-26 18:52:04 +0300

    Проверка теста

--- store/store.go
+++ store/store.go
@@ -10,16 +10,14 @@ import (
 	"time"
 )
 
-// StoreDB представляет структуру для работы с базой данных.
 type StoreDB struct {
 	db *sql.DB
 }
 
-// InitDatabase инициализирует соединение с базой данных и создает таблицу, если она не существует.
 func InitDatabase(DatabasePath string) (*StoreDB, error) {
 	db, err := sql.Open("pgx", DatabasePath)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка при открытии базы данных: %w", err)
+		return nil, fmt.Errorf("error opening db: %w", err)
 	}
 
 	storeDB := new(StoreDB)
@@ -28,14 +26,13 @@ func InitDatabase(DatabasePath string) (*StoreDB, error) {
 	if DatabasePath != "" {
 		err = createTable(db)
 		if err != nil {
-			return nil, fmt.Errorf("ошибка при создании таблицы в базе данных: %w", err)
+			return nil, fmt.Errorf("error creae table db: %w", err)
 		}
 	}
 
 	return storeDB, nil
 }
 
-// Create добавляет новую запись в таблицу urls.
 func (s *StoreDB) Create(originalURL, shortURL, UserID string) error {
 	query := `
         INSERT INTO urls (short_id, original_url, userID) 
@@ -43,12 +40,11 @@ func (s *StoreDB) Create(originalURL, shortURL, UserID string) error {
     `
 	_, err := s.db.Exec(query, shortURL, originalURL, UserID)
 	if err != nil {
-		return fmt.Errorf("ошибка при добавлении новой записи: %w", err)
+		return err
 	}
 	return nil
 }
 
-// createTable создает таблицу urls, если она не существует, и индекс на поле original_url.
 func createTable(db *sql.DB) error {
 	query := `CREATE TABLE IF NOT EXISTS urls (
 		id SERIAL PRIMARY KEY,
@@ -59,7 +55,7 @@ func createTable(db *sql.DB) error {
     	deletedFlag BOOLEAN DEFAULT FALSE
 	);
 	DO $$ 
-	BEGIN 	
+	BEGIN 
    	 IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE tablename = 'urls' AND indexname = 'idx_original_url') THEN
         CREATE UNIQUE INDEX idx_original_url ON urls(original_url);
     END IF;
@@ -67,18 +63,17 @@ func createTable(db *sql.DB) error {
 
 	_, err := db.Exec(query)
 	if err != nil {
-		return fmt.Errorf("ошибка при создании таблицы: %w", err)
+		return err
 	}
 
 	return nil
 }
 
-// GetFull возвращает все ссылки пользователя в виде массива карт.
 func (s *StoreDB) GetFull(userID string, BaseURL string) ([]map[string]string, error) {
 	query := `SELECT short_id, original_url, deletedFlag FROM urls WHERE userID = $1`
 	rows, err := s.db.Query(query, userID)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка при получении ссылок: %w", err)
+		return nil, fmt.Errorf("failed to get links: %w", err)
 	}
 	defer rows.Close()
 
@@ -90,7 +85,7 @@ func (s *StoreDB) GetFull(userID string, BaseURL string) ([]map[string]string, e
 			deletedFlag bool
 		)
 		if err = rows.Scan(&shortID, &originalURL, &deletedFlag); err != nil {
-			return nil, fmt.Errorf("ошибка при сканировании строки: %w", err)
+			return nil, err
 		}
 		if deletedFlag {
 			err = errors.New(http.StatusText(http.StatusGone))
@@ -101,28 +96,26 @@ func (s *StoreDB) GetFull(userID string, BaseURL string) ([]map[string]string, e
 		urls = append(urls, urlMap)
 	}
 	if err = rows.Err(); err != nil {
-		return nil, fmt.Errorf("ошибка при итерации по строкам ссылок: %w", err)
+		return nil, fmt.Errorf("error during iteration through link rows: %w", err)
 	}
 
 	return urls, nil
 }
 
-// DeleteURLs помечает ссылки как удаленные для определенного пользователя.
 func (s *StoreDB) DeleteURLs(userID string, shortURL string, updateChan chan<- string) error {
 	query := `
 		UPDATE urls
 		SET deletedFlag = true
-		WHERE short_id = $1 AND userID = $2`
+		WHERE short_id = $1 and  userID = $2`
 
 	_, err := s.db.Exec(query, shortURL, userID)
 	if err != nil {
-		return fmt.Errorf("ошибка при обновлении записи: %w", err)
+		return err
 	}
 	updateChan <- shortURL
 	return nil
 }
 
-// Get возвращает либо оригинальную ссылку по сокращенной, либо сокращенную ссылку по оригинальной.
 func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
 	field1 := "original_url"
 	field2 := "short_id"
@@ -145,7 +138,7 @@ func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
 	)
 	err := s.db.QueryRow(query, field).Scan(&answer, &deletedFlag)
 	if err != nil {
-		return "", fmt.Errorf("ошибка при получении данных: %w", err)
+		return "", err
 	}
 
 	if deletedFlag {
@@ -153,15 +146,14 @@ func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
 		return "", err
 	}
 
-	return answer, nil
+	return answer, err
 }
 
-// PingStore проверяет доступность базы данных.
 func (s *StoreDB) PingStore() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	if err := s.db.PingContext(ctx); err != nil {
-		return fmt.Errorf("ошибка при пинге базы данных: %w", err)
+		return fmt.Errorf("pinging db-store: %w", err)
 	}
 	return nil
 }
