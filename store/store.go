a7f223531a57996a0126c983658aed9581a416f9
Renal37 <renalplaycsgo@gmail.com>
2024-08-26 03:30:26 +0300

    Проверка

--- store/store.go
+++ store/store.go
@@ -8,94 +8,96 @@ import (
 	"time"
 )
 
-// StoreDB представляет структуру базы данных
 type StoreDB struct {
 	db *sql.DB
 }
 
-// InitDatabase инициализирует базу данных по заданному пути
-func InitDatabase(databasePath string) (*StoreDB, error) {
-	if databasePath == "" {
-		return nil, fmt.Errorf("путь к базе данных не может быть пустым")
-	}
-
-	// Подключаемся к базе данных
-	db, err := sql.Open("pgx", databasePath)
+func InitDatabase(DatabasePath string) (*StoreDB, error) {
+	db, err := sql.Open("pgx", DatabasePath)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка при открытии базы данных: %w", err)
+		return nil, fmt.Errorf("error opening db: %w", err)
 	}
 
-	// Создаем таблицу, если требуется
-	if err := createTable(db); err != nil {
-		return nil, fmt.Errorf("ошибка при создании таблицы в базе данных: %w", err)
-	}
+	storeDB := new(StoreDB)
+	storeDB.db = db
 
-	return &StoreDB{db: db}, nil
-}
-
-// Create добавляет новую запись с оригинальным и коротким URL
-func (s *StoreDB) Create(originalURL, shortURL string) error {
-	if originalURL == "" || shortURL == "" {
-		return fmt.Errorf("оригинальный URL и короткий URL не могут быть пустыми")
-	}
-
-	query := `INSERT INTO urls (short_id, original_url) VALUES ($1, $2)`
-	if _, err := s.db.Exec(query, shortURL, originalURL); err != nil {
-		return fmt.Errorf("ошибка при добавлении записи в базу данных: %w", err)
-	}
-	return nil
-}
-
-// createTable создает таблицу urls, если она еще не существует
-func createTable(db *sql.DB) error {
-	query := `
-		CREATE TABLE IF NOT EXISTS urls (
-			id SERIAL PRIMARY KEY,
-			short_id VARCHAR(256) NOT NULL UNIQUE,
-			original_url TEXT NOT NULL,
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		);
-		CREATE UNIQUE INDEX IF NOT EXISTS idx_original_url ON urls(original_url);
-	`
-	if _, err := db.Exec(query); err != nil {
-		return fmt.Errorf("ошибка при создании таблицы: %w", err)
-	}
-	return nil
-}
-
-// Get возвращает оригинальный или короткий URL на основе переданного значения
-func (s *StoreDB) Get(shortURL, originalURL string) (string, error) {
-	if shortURL == "" && originalURL == "" {
-		return "", fmt.Errorf("короткий URL или оригинальный URL должны быть указаны")
-	}
-
-	// Определяем, по какому полю будет происходить поиск
-	field1, field2, fieldValue := "original_url", "short_id", shortURL
-	if shortURL == "" {
-		field1, field2, fieldValue = "short_id", "original_url", originalURL
-	}
-
-	query := fmt.Sprintf(`SELECT %s FROM urls WHERE %s = $1`, field1, field2)
-
-	var result string
-	if err := s.db.QueryRow(query, fieldValue).Scan(&result); err != nil {
-		if err == sql.ErrNoRows {
-			return "", fmt.Errorf("запись не найдена")
+	if DatabasePath != "" {
+		err = createTable(db)
+		if err != nil {
+			return nil, fmt.Errorf("error creae table db: %w", err)
 		}
-		return "", fmt.Errorf("ошибка при получении данных из базы: %w", err)
 	}
 
-	return result, nil
+	return storeDB, nil
 }
 
-// PingStore проверяет доступность базы данных
+func (s *StoreDB) Create(originalURL, shortURL string) error {
+	query := `
+        INSERT INTO urls (short_id, original_url) 
+        VALUES ($1, $2)
+    `
+	_, err := s.db.Exec(query, shortURL, originalURL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createTable(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS urls (
+		id SERIAL PRIMARY KEY,
+		short_id VARCHAR(256) NOT NULL UNIQUE,
+		original_url TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+	DO $$ 
+	BEGIN 
+   	 IF NOT EXISTS (SELECT 1 FROM pg_indexes WHERE tablename = 'urls' AND indexname = 'idx_original_url') THEN
+        CREATE UNIQUE INDEX idx_original_url ON urls(original_url);
+    END IF;
+	END $$;`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StoreDB) Get(shortURL string, originalURL string) (string, error) {
+	field1 := "original_url"
+	field2 := "short_id"
+	field := shortURL
+	if shortURL == "" {
+		field2 = "original_url"
+		field1 = "short_id"
+		field = originalURL
+	}
+
+	query := fmt.Sprintf(`
+        SELECT %s 
+        FROM urls 
+        WHERE %s = $1
+    `, field1, field2)
+
+	var answer string
+	err := s.db.QueryRow(query, field).Scan(&answer)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", err
+		}
+		return "", err
+	}
+
+	return answer, err
+}
+
 func (s *StoreDB) PingStore() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
-
 	if err := s.db.PingContext(ctx); err != nil {
-		return fmt.Errorf("ошибка при проверке доступности базы данных: %w", err)
+		return fmt.Errorf("pinging db-store: %w", err)
 	}
-
 	return nil
 }
