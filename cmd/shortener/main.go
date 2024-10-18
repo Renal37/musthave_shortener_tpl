b15e1a421bf59dd99e7524cdc6dda20ed46c7249
Renal37 <renalplaycsgo@gmail.com>
2024-10-18 19:03:47 +0300

    iter16

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,25 +10,11 @@
 		"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
 	)
 
-	const (
-		addr    = ":8080"  // адрес сервера
-		maxSize = 10000000 // будем растить слайс до 10 миллионов элементов
-	)
 
-	func foo() {
-		// полезная нагрузка
-		for {
-			var s []int
-			for i := 0; i < maxSize; i++ {
-				s = append(s, i)
-			}
-		}
-	}
 	func main() {
 		addrConfig := config.InitConfig()
 		storageInstance := storage.NewStorage()
 		appInstance := app.NewApp(storageInstance, addrConfig)
-		go foo()
 		// Запуск pprof сервера на порту 6060
 		go func() {
 			log.Println(http.ListenAndServe("localhost:6060", nil))
