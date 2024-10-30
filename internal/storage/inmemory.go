f06ff6d5dfe1ed35f49e91412a304872bd02188d
Renal37 <renalplaycsgo@gmail.com>
2024-10-30 18:31:30 +0300

    проверка на тест

--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -3,16 +3,20 @@ package storage
 type Storage struct {
     URLs map[string]string
 }
+
 // Функция создания нового экземпляра хранилища
 func NewStorage() *Storage {
     return &Storage{
         URLs: make(map[string]string),
     }
 }
+
 // Функция установки значения по ключу
-func (s *Storage) Set(key string, value string) {
+func (s *Storage) Set(key string, value string) error {
     s.URLs[key] = value
+    return nil // Возвращаем nil, так как нет ошибок
 }
+
 // Функция получения значения по ключу
 func (s *Storage) Get(key string) (string, bool) {
     value, exists := s.URLs[key]
