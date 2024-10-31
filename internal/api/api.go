a04178907d27d1d9964f31ac96a1b74a025737a0
Renal37 <renalplaycsgo@gmail.com>
2024-10-31 11:01:02 +0300

    Проверка

--- internal/api/api.go
+++ internal/api/api.go
@@ -21,6 +21,7 @@ import (
 type RestAPI struct {
 	Shortener *services.ShortenerService // Сервис для сокращения URL.
 }
+
 // StartRestAPI запускает HTTP-сервер REST API для обработки запросов сокращения URL.
 //
 // Сервер создает и настраивает маршруты с использованием middleware и предоставляет эндпоинты
@@ -35,63 +36,64 @@ type RestAPI struct {
 //   - storage: Объект Storage для хранения данных (предполагается реализация интерфейса хранилища).
 //
 // Пример использования:
-//   go func() {
-//       err := StartRestAPI(":8080", "http://example.com", "info", db, false, storage)
-//       if err != nil {
-//           log.Fatalf("Ошибка запуска API: %v", err)
-//       }
-//   }()
+//
+//	go func() {
+//	    err := StartRestAPI(":8080", "http://example.com", "info", db, false, storage)
+//	    if err != nil {
+//	        log.Fatalf("Ошибка запуска API: %v", err)
+//	    }
+//	}()
 //
 // Известные ограничения:
-//   • Если сервер запущен, его завершение произойдет при получении сигнала остановки от системы.
-//   • Время завершения при остановке ограничено 5 секундами (по умолчанию).
+//   - Если сервер запущен, его завершение произойдет при получении сигнала остановки от системы.
+//   - Время завершения при остановке ограничено 5 секундами (по умолчанию).
 //
 // BUG(Автор): Текущее логирование ограничено уровнем info; более детализированные уровни требуют дальнейшей настройки.
 // BUG(Автор): В конфигурации сервера может отсутствовать поддержка HTTPS.
 func StartRestAPI(ServerAddr, BaseURL string, LogLevel string, db *repository.StoreDB, dbDNSTurn bool, storage *storage.Storage) error {
-    if err := logger.Initialize(LogLevel); err != nil {
-        return err
-    }
-    logger.Log.Info("Running server", zap.String("address", ServerAddr))
-    storageShortener := services.NewShortenerService(BaseURL, storage, db, dbDNSTurn)
+	if err := logger.Initialize(LogLevel); err != nil {
+		return err
+	}
+	logger.Log.Info("Running server", zap.String("address", ServerAddr))
+	storageShortener := services.NewShortenerService(BaseURL, storage, db, dbDNSTurn)
 
-    api := &RestAPI{
-        Shortener: storageShortener,
-    }
+	api := &RestAPI{
+		Shortener: storageShortener,
+	}
 
-    gin.SetMode(gin.ReleaseMode)
-    r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 
-    r.Use(
-        gin.Recovery(),
-        middleware.LoggerMiddleware(logger.Log),
-        middleware.CompressMiddleware(),
-        middleware.AuthorizationMiddleware(),
-    )
+	r.Use(
+		gin.Recovery(),
+		middleware.LoggerMiddleware(logger.Log),
+		middleware.CompressMiddleware(),
+		middleware.AuthorizationMiddleware(),
+	)
 
-    api.setRoutes(r) // Настройка маршрутов API
+	api.setRoutes(r) // Настройка маршрутов API
 
-    server := &http.Server{
-        Addr:    ServerAddr,
-        Handler: r,
-    }
+	server := &http.Server{
+		Addr:    ServerAddr,
+		Handler: r,
+	}
 
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, os.Interrupt)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
 
-    go func() {
-        if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("Ошибка при запуске сервера: %v", err)
-        }
-    }()
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Ошибка при запуске сервера: %v", err)
+		}
+	}()
 
-    <-quit
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-    if err := server.Shutdown(ctx); err != nil {
-        log.Fatalf("Ошибка при остановке сервера: %v\n", err)
-    }
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Ошибка при остановке сервера: %v\n", err)
+	}
 
-    return nil
+	return nil
 }
