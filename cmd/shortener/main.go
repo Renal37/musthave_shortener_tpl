b22c4e779552d22d5d8f847535579ab4dd8c485c
Renal37 <renalplaycsgo@gmail.com>
2024-11-03 16:42:27 +0300

    proff

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -10,14 +11,20 @@ import (
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
 )
 
+var profilingEnabled = flag.Bool("profiling", false, "Enable profiling server")
+
 func main() {
+	flag.Parse()
+
 	addrConfig := config.InitConfig()
 	storageInstance := storage.NewStorage()
 	appInstance := app.NewApp(storageInstance, addrConfig)
-	// Запуск pprof сервера на порту 6060
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+
+	if *profilingEnabled {
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
 
 	appInstance.Start()
 	appInstance.Stop()
