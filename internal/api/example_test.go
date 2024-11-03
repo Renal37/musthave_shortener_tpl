4752c991cdf333d8b55403ce537802c82334c44b
Renal37 <renalplaycsgo@gmail.com>
2024-11-03 20:23:22 +0300

    iter18

--- internal/api/example_test.go
+++ internal/api/example_test.go
@@ -2,119 +2,70 @@ package api_test
 
 import (
 	"bytes"
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strings"
-	"testing"
 
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/api"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/services"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 )
 
-// Test shortening URLs with a handler
-func TestShortenURLHandler(t *testing.T) {
+// Example для обработки сокращения URL
+func ShortenURLHandlers() {
+	// Инициализация зависимостей
 	storageInstance := storage.NewStorage()
 	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
 	handler := api.RestAPI{Shortener: storageShortener}
 
-	tests := []struct {
-		name         string
-		body         string
-		expectedCode int
-	}{
-		{"Valid URL", "https://practicum.yandex.ru/", 201},
-	}
+	// Настройка роутера Gin
+	r := gin.Default()
+	r.POST("/", handler.ShortenURLHandler)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := gin.Default()
-			r.POST("/", handler.ShortenURLHandler)
+	// Создание HTTP-запроса
+	body := "https://practicum.yandex.ru/"
+	request := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString(body))
+	w := httptest.NewRecorder()
 
-			request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tt.body))
-			w := httptest.NewRecorder()
-			r.ServeHTTP(w, request)
+	// Выполнение запроса
+	r.ServeHTTP(w, request)
 
-			// Check status code
-			assert.Equal(t, tt.expectedCode, w.Code)
+	// Выводим результат
+	fmt.Println(w.Code)
+	fmt.Println(w.Body.String())
 
-			// Check if response body is a valid URL
-			assert.Regexp(t, `^http://localhost:8080/[0-9a-fA-F-]{36}$`, w.Body.String())
-		})
-	}
+	// Output:
+	// 201
+	// http://localhost:8080/<UUID>
 }
 
-// Test shortening URLs with JSON
-func TestShortenURLHandlerJSON(t *testing.T) {
+// Example для редиректа на оригинальный URL
+func RedirectToOriginalURLHandlers() {
+	// Инициализация зависимостей
 	storageInstance := storage.NewStorage()
 	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
 	handler := api.RestAPI{Shortener: storageShortener}
 
-	tests := []struct {
-		name         string
-		body         map[string]string
-		expectedCode int
-	}{
-		{"Valid JSON URL", map[string]string{"url": "https://practicum.yandex.ru"}, 201},
-	}
+	// Добавляем URL в хранилище
+	storageInstance.Set("test_id", "https://practicum.yandex.ru/")
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := gin.Default()
-			r.POST("/api/shorten", handler.ShortenURLJSON)
+	// Настройка роутера Gin
+	r := gin.Default()
+	r.GET("/:id", handler.RedirectToOriginalURL)
 
-			jsonBody, _ := json.Marshal(tt.body)
-			request := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewReader(jsonBody))
-			request.Header.Set("Content-Type", "application/json")
-			w := httptest.NewRecorder()
+	// Создание HTTP-запроса
+	request := httptest.NewRequest(http.MethodGet, "/test_id", nil)
+	w := httptest.NewRecorder()
 
-			r.ServeHTTP(w, request)
+	// Выполнение запроса
+	r.ServeHTTP(w, request)
 
-			// Check status code
-			assert.Equal(t, tt.expectedCode, w.Code)
+	// Выводим результат
+	fmt.Println(w.Code)
+	fmt.Println(w.Header().Get("Location"))
 
-			// Validate response JSON structure
-			var result map[string]string
-			if err := json.Unmarshal(w.Body.Bytes(), &result); err == nil {
-				assert.Regexp(t, `^http://localhost:8080/[0-9a-fA-F-]{36}$`, result["result"])
-			}
-		})
-	}
-}
-
-// Test redirecting to the original URL
-func TestRedirectToOriginalURLHandler(t *testing.T) {
-	storageInstance := storage.NewStorage()
-	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
-	handler := api.RestAPI{Shortener: storageShortener}
-
-	tests := []struct {
-		name       string
-		shortID    string
-		original   string
-		statusCode int
-	}{
-		{"Redirect to original URL", "test_id", "https://practicum.yandex.ru/", 307},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			storageInstance.Set(tt.shortID, tt.original)
-
-			r := gin.Default()
-			r.GET("/:id", handler.RedirectToOriginalURL)
-
-			request := httptest.NewRequest(http.MethodGet, "/"+tt.shortID, nil)
-			w := httptest.NewRecorder()
-
-			r.ServeHTTP(w, request)
-
-			// Check status code
-			assert.Equal(t, tt.statusCode, w.Code)
-			assert.Equal(t, tt.original, w.Header().Get("Location"))
-		})
-	}
+	// Output:
+	// 307
+	// https://practicum.yandex.ru/
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,17 +2,19 @@ package config
 
 import (
 	"flag"
-	"github.com/caarlos0/env/v6"
 	"sync"
+
+	"github.com/caarlos0/env/v6"
 )
 
 // Config представляет собой структуру для хранения конфигурационных параметров приложения.
 type Config struct {
-	ServerAddr string `env:"SERVER_ADDRESS"`    // Адрес и порт сервера
-	BaseURL    string `env:"BASE_URL"`          // Базовый URL
-	LogLevel   string `env:"FLAG_LOG_LEVEL"`    // Уровень логирования
-	FilePath   string `env:"FILE_STORAGE_PATH"` // Путь к файлу для хранения
-	DBPath     string `env:"DB_PATH"`           // Путь к базе данных
+	ServerAddr  string `env:"SERVER_ADDRESS"`    // Адрес и порт сервера
+	BaseURL     string `env:"BASE_URL"`          // Базовый URL
+	LogLevel    string `env:"LOG_LEVEL"`         // Уровень логирования
+	FilePath    string `env:"FILE_STORAGE_PATH"` // Путь к файлу для хранения
+	DBPath      string `env:"DB_PATH"`           // Путь к базе данных
+	EnablePprof bool   `env:"ENABLE_PPROF"`      // Включение профайлинга
 }
 
 var once sync.Once
@@ -20,27 +22,27 @@ var once sync.Once
 // InitConfig инициализирует конфигурацию, загружая параметры из переменных окружения и флагов командной строки.
 func InitConfig() *Config {
 	config := &Config{
-		ServerAddr: "localhost:8080",        // Значение по умолчанию для адреса сервера
-		BaseURL:    "http://localhost:8080", // Значение по умолчанию для базового URL
-		LogLevel:   "info",                  // Значение по умолчанию для уровня логирования
-		FilePath:   "short-url-db.json",     // Значение по умолчанию для пути к файлу
-		DBPath:     "",                      // Значение по умолчанию для пути к базе данных
+		ServerAddr:  "localhost:8080",        // Значение по умолчанию для адреса сервера
+		BaseURL:     "http://localhost:8080", // Значение по умолчанию для базового URL
+		LogLevel:    "info",                  // Значение по умолчанию для уровня логирования
+		FilePath:    "short-url-db.json",     // Значение по умолчанию для пути к файлу
+		DBPath:      "",                      // Значение по умолчанию для пути к базе данных
+		EnablePprof: false,                   // Значение по умолчанию для включения профайлинга
 	}
 
 	// Определяем флаги командной строки
 	once.Do(func() {
 		flag.StringVar(&config.ServerAddr, "a", config.ServerAddr, "address and port to run api")
-		flag.StringVar(&config.BaseURL, "b", config.BaseURL, "base URL")
-		flag.StringVar(&config.LogLevel, "c", config.LogLevel, "log level")
-		flag.StringVar(&config.FilePath, "f", config.FilePath, "path to file for storage")
-		flag.StringVar(&config.DBPath, "d", config.DBPath, "path to database")
-
+		flag.StringVar(&config.BaseURL, "-b", config.BaseURL, "base URL")
+		flag.StringVar(&config.LogLevel, "-c", config.LogLevel, "log level")
+		flag.StringVar(&config.FilePath, "-f", config.FilePath, "path to file for storage")
+		flag.StringVar(&config.DBPath, "-d", config.DBPath, "path to database")
+		flag.BoolVar(&config.EnablePprof, "e", config.EnablePprof, "enable profiling")
 		flag.Parse() // Парсим флаги командной строки
 	})
 
 	// Загружаем параметры из переменных окружения
-	err := env.Parse(config)
-	if err != nil {
+	if err := env.Parse(config); err != nil {
 		panic(err) // Завершаем программу, если произошла ошибка
 	}
 
