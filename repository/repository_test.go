43a1300612520d44aab650ad844df7706b81f805
Renal37 <renalplaycsgo@gmail.com>
2024-11-03 19:18:49 +0300

    iter18

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -9,6 +9,103 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestStoreDB_Create(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+	mock.ExpectExec("INSERT INTO urls").
+		WithArgs("shortURL", "originalURL", "userID").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = store.Create("originalURL", "shortURL", "userID")
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestStoreDB_GetFull(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+	rows := sqlmock.NewRows([]string{"short_id", "original_url", "deletedFlag"}).
+		AddRow("shortURL1", "originalURL1", false).
+		AddRow("shortURL2", "originalURL2", false)
+
+	mock.ExpectQuery("SELECT short_id, original_url, deletedFlag FROM urls").
+		WithArgs("userID").
+		WillReturnRows(rows)
+
+	result, err := store.GetFull("userID", "http://localhost:8080")
+	assert.NoError(t, err)
+	assert.Len(t, result, 2)
+	assert.Equal(t, "http://localhost:8080/shortURL1", result[0]["short_url"])
+}
+
+func TestStoreDB_DeleteURLs(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+	updateChan := make(chan string, 1)
+
+	mock.ExpectExec("UPDATE urls").
+		WithArgs("shortURL", "userID").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = store.DeleteURLs("userID", "shortURL", updateChan)
+	assert.NoError(t, err)
+	assert.Equal(t, "shortURL", <-updateChan)
+}
+
+func TestStoreDB_Get(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+	rows := sqlmock.NewRows([]string{"original_url", "deletedFlag"}).AddRow("originalURL", false)
+
+	mock.ExpectQuery("SELECT original_url, deletedFlag FROM urls WHERE short_id =").
+		WithArgs("shortURL").
+		WillReturnRows(rows)
+
+	originalURL, err := store.Get("shortURL", "")
+	assert.NoError(t, err)
+	assert.Equal(t, "originalURL", originalURL)
+}
+
+func TestStoreDB_PingStore(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+	mock.ExpectPing()
+
+	err = store.PingStore()
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+func TestStoreDB_PingStore_Error(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := &StoreDB{db: db}
+
+	// Установите ожидание на пинг, который вернет ошибку
+	mock.ExpectPing().WillReturnError(errors.New("ping error"))
+
+	err = store.PingStore()
+	assert.Error(t, err)
+	assert.Equal(t, "pinging db-store: ping error", err.Error()) // Проверяем текст ошибки
+	assert.NoError(t, mock.ExpectationsWereMet())                // Проверяем, что все ожидания выполнены
+}
+
 func TestStoreDB_Create_Error(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	require.NoError(t, err)
@@ -73,19 +170,5 @@ func TestStoreDB_Get_Error(t *testing.T) {
 	originalURL, err := store.Get("shortURL", "")
 	assert.Error(t, err)
 	assert.Empty(t, originalURL)
-	assert.NoError(t, mock.ExpectationsWereMet())
-}
-
-func TestStoreDB_PingStore_Error(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer db.Close()
-
-	store := &StoreDB{db: db}
-	mock.ExpectPing().WillReturnError(errors.New("ping error"))
-
-	err = store.PingStore()
-	assert.Error(t, err)
-	assert.Equal(t, "pinging db-store: ping error", err.Error())
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
