2e0fd517b0bddb7618cc768cb418565b3a72e767
Renal37 <renalplaycsgo@gmail.com>
2024-11-03 17:06:08 +0300

    iter18

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,25 +1,25 @@
 package api
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
+    "context"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "time"
 
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/logger"
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/middleware"
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/services"
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
-	"github.com/Renal37/musthave_shortener_tpl.git/repository"
-	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
+    "github.com/Renal37/musthave_shortener_tpl.git/internal/logger"
+    "github.com/Renal37/musthave_shortener_tpl.git/internal/middleware"
+    "github.com/Renal37/musthave_shortener_tpl.git/internal/services"
+    "github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
+    "github.com/Renal37/musthave_shortener_tpl.git/repository"
+    "github.com/gin-gonic/gin"
+    "go.uber.org/zap"
 )
 
 // RestAPI представляет собой структуру для REST API.
 type RestAPI struct {
-	Shortener *services.ShortenerService // Сервис для сокращения URL.
+    Shortener *services.ShortenerService // Сервис для сокращения URL.
 }
 
 // StartRestAPI запускает HTTP-сервер REST API для обработки запросов сокращения URL.
@@ -51,49 +51,53 @@ type RestAPI struct {
 // BUG(Автор): Текущее логирование ограничено уровнем info; более детализированные уровни требуют дальнейшей настройки.
 // BUG(Автор): В конфигурации сервера может отсутствовать поддержка HTTPS.
 func StartRestAPI(ServerAddr, BaseURL string, LogLevel string, db *repository.StoreDB, dbDNSTurn bool, storage *storage.Storage) error {
-	if err := logger.Initialize(LogLevel); err != nil {
-		return err
-	}
-	logger.Log.Info("Running server", zap.String("address", ServerAddr))
-	storageShortener := services.NewShortenerService(BaseURL, storage, db, dbDNSTurn)
+    if err := logger.Initialize(LogLevel); err != nil {
+        return err
+    }
+    logger.Log.Info("Running server", zap.String("address", ServerAddr))
+    storageShortener := services.NewShortenerService(BaseURL, storage, db, dbDNSTurn)
 
-	api := &RestAPI{
-		Shortener: storageShortener,
-	}
+    api := &RestAPI{
+        Shortener: storageShortener,
+    }
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
+    gin.SetMode(gin.ReleaseMode)
+    r := gin.Default()
 
-	r.Use(
-		gin.Recovery(),
-		middleware.LoggerMiddleware(logger.Log),
-		middleware.CompressMiddleware(),
-		middleware.AuthorizationMiddleware(),
-	)
+    r.Use(
+        gin.Recovery(),
+        middleware.LoggerMiddleware(logger.Log),
+        middleware.CompressMiddleware(),
+        middleware.AuthorizationMiddleware(),
+    )
 
-	api.setRoutes(r) // Настройка маршрутов API
+    api.setRoutes(r) // Настройка маршрутов API
 
-	server := &http.Server{
-		Addr:    ServerAddr,
-		Handler: r,
-	}
+    return startServer(ServerAddr, r)
+}
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+func startServer(ServerAddr string, r *gin.Engine) error {
+    server := &http.Server{
+        Addr:    ServerAddr,
+        Handler: r,
+    }
 
-	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Ошибка при запуске сервера: %v", err)
-		}
-	}()
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, os.Interrupt)
 
-	<-quit
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+    go func() {
+        if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+            log.Fatalf("Ошибка при запуске сервера: %v", err)
+        }
+    }()
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Ошибка при остановке сервера: %v\n", err)
-	}
+    <-quit
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
 
-	return nil
+    if err := server.Shutdown(ctx); err != nil {
+        log.Fatalf("Ошибка при остановке сервера: %v\n", err)
+    }
+
+    return nil
 }
