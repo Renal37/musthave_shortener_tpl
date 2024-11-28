25ab406220687a953a2d3127aa6c99cba00a096d
Renal37 <renalplaycsgo@gmail.com>
2024-11-28 15:29:49 +0300

    iter23TSL

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/Renal37/musthave_shortener_tpl.git/internal/api"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/app"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/config"
 	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -34,9 +37,10 @@ func main() {
 	fmt.Printf("Build date: %s\n", buildDate)
 	fmt.Printf("Build commit: %s\n", buildCommit)
 
-	addrConfig := config.InitConfig()
-	storageInstance := storage.NewStorage()
-	appInstance := app.NewApp(storageInstance, addrConfig)
+	addrConfig := config.InitConfig()                      // Инициализация конфигурации
+	storageInstance := storage.NewStorage()                // Создание хранилища
+	appInstance := app.NewApp(storageInstance, addrConfig) // Создание приложения
+
 	// Запуск pprof сервера на порту 6060, если включен флаг
 	if addrConfig.EnablePprof == "true" {
 		go func() {
@@ -44,8 +48,31 @@ func main() {
 		}()
 	}
 
+	// Настройка TLS конфигурации
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// Создание экземпляра gin.Engine
+	r := gin.Default()
+
+	// Настройка маршрутов
+	apiInstance := &api.RestAPI{} // Создайте экземпляр RestAPI
+	apiInstance.SetRoutes(r)
+
+	// Создание HTTPS сервера
+	server := &http.Server{
+		Addr:      addrConfig.ServerAddr,
+		Handler:   r, // Используем gin.Engine в качестве обработчика
+		TLSConfig: tlsConfig,
+	}
+
+	// Запуск HTTPS сервера
+	err := server.ListenAndServeTLS("server.crt", "server.key")
+	if err != nil {
+		log.Fatalf("Error starting HTTPS server: %v", err)
+	}
+
 	appInstance.Start()
 	appInstance.Stop()
 }
-
-// ./shortener
