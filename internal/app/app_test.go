8038a231027926d03de1f9f122dd82983a8e06db
Renal37 <renalplaycsgo@gmail.com>
2024-11-30 15:39:03 +0300

    iter23

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -42,7 +42,6 @@ func TestNewApp(t *testing.T) {
 	}
 }
 
-// Тест для метода Start с реальным хранилищем
 // Тест для метода Start
 func TestApp_Start(t *testing.T) {
 	// Настройка конфигурации и хранилища для теста
@@ -66,6 +65,28 @@ func TestApp_Start(t *testing.T) {
 	}()
 
 	app.Start()
+}
+
+// Тест для метода Stop
+func TestApp_Stop(t *testing.T) {
+	// Настройка конфигурации и хранилища для теста
+	mockStorage := &storage.Storage{}
+	mockConfig := &config.Config{
+		DBPath:   "test_db_path",
+		FilePath: "test_file_path",
+	}
+
+	// Создаем экземпляр App
+	app := NewApp(mockStorage, mockConfig)
+
+	// Вызываем метод Stop и проверяем, что он не вызывает паники
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Stop вызвал панику: %v", r)
+		}
+	}()
+
+	app.Stop()
 }
 
 // Тест для метода Start с ошибкой инициализации базы данных
@@ -97,34 +118,6 @@ func TestApp_Start_ErrorInitDatabase(t *testing.T) {
 	if err == nil {
 		t.Errorf("Ожидалась ошибка при инициализации базы данных, но её не было")
 	}
-}
-
-// Тест для метода Stop с реальным хранилищем
-func TestApp_Stop(t *testing.T) {
-	// Настройка конфигурации для теста
-	mockConfig := &config.Config{
-		DBPath:     "test_db_path",
-		FilePath:   "test_file_path",
-		ServerAddr: "localhost:8080",
-		BaseURL:    "http://localhost",
-		LogLevel:   "info",
-	}
-
-	// Создаем экземпляр хранилища через storage.NewStorage()
-	mockStorage := storage.NewStorage()
-
-	// Создаем экземпляр App
-	app := NewApp(mockStorage, mockConfig)
-
-	// Проверка того, что метод Stop не вызывает панику
-	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("Stop вызвал панику: %v", r)
-		}
-	}()
-
-	// Запуск и остановка приложения
-	app.Stop()
 }
 
 // Тест для метода UseDatabase
