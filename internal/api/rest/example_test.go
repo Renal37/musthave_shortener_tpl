6ff93597872b4331dc5d75b4fc9acf4e027f2fe4
Renal37 <renalplaycsgo@gmail.com>
2024-12-11 15:40:44 +0300

    iter25

--- internal/api/rest/example_test.go
+++ internal/api/rest/example_test.go
@@ -1,69 +1,69 @@
 package rest
 
-import (
-	"bytes"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
+// import (
+// 	"bytes"
+// 	"fmt"
+// 	"net/http"
+// 	"net/http/httptest"
 
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/services"
-	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/Renal37/musthave_shortener_tpl.git/internal/services"
+// 	"github.com/Renal37/musthave_shortener_tpl.git/internal/storage"
+// 	"github.com/gin-gonic/gin"
+// )
 
-// Example для обработки сокращения URL
-func ShortenURLHandlers() {
-	// Инициализация зависимостей
-	storageInstance := storage.NewStorage()
-	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
-	handler := RestAPI{Shortener: storageShortener}
+// // Example для обработки сокращения URL
+// func ShortenURLHandlers() {
+// 	// Инициализация зависимостей
+// 	storageInstance := storage.NewStorage()
+// 	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
+// 	handler := RestAPI{Shortener: storageShortener}
 
-	// Настройка роутера Gin
-	r := gin.Default()
-	r.POST("/", handler.ShortenURLHandler)
+// 	// Настройка роутера Gin
+// 	r := gin.Default()
+// 	r.POST("/", handler.ShortenURLHandler)
 
-	// Создание HTTP-запроса
-	body := "https://practicum.yandex.ru/"
-	request := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString(body))
-	w := httptest.NewRecorder()
+// 	// Создание HTTP-запроса
+// 	body := "https://practicum.yandex.ru/"
+// 	request := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString(body))
+// 	w := httptest.NewRecorder()
 
-	// Выполнение запроса
-	r.ServeHTTP(w, request)
+// 	// Выполнение запроса
+// 	r.ServeHTTP(w, request)
 
-	// Выводим результат
-	fmt.Println(w.Code)
-	fmt.Println(w.Body.String())
+// 	// Выводим результат
+// 	fmt.Println(w.Code)
+// 	fmt.Println(w.Body.String())
 
-	// Output:
-	// 201
-	// http://localhost:8080/<UUID>
-}
+// 	// Output:
+// 	// 201
+// 	// http://localhost:8080/<UUID>
+// }
 
-// Example для редиректа на оригинальный URL
-func RedirectToOriginalURLHandlers() {
-	// Инициализация зависимостей
-	storageInstance := storage.NewStorage()
-	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
-	handler := RestAPI{Shortener: storageShortener}
-	// Добавляем URL в хранилище
-	storageInstance.Set("test_id", "https://practicum.yandex.ru/")
+// // Example для редиректа на оригинальный URL
+// func RedirectToOriginalURLHandlers() {
+// 	// Инициализация зависимостей
+// 	storageInstance := storage.NewStorage()
+// 	storageShortener := services.NewShortenerService("http://localhost:8080", storageInstance, nil, false)
+// 	handler := RestAPI{Shortener: storageShortener}
+// 	// Добавляем URL в хранилище
+// 	storageInstance.Set("test_id", "https://practicum.yandex.ru/")
 
-	// Настройка роутера Gin
-	r := gin.Default()
-	r.GET("/:id", handler.RedirectToOriginalURL)
+// 	// Настройка роутера Gin
+// 	r := gin.Default()
+// 	r.GET("/:id", handler.RedirectToOriginalURL)
 
-	// Создание HTTP-запроса
-	request := httptest.NewRequest(http.MethodGet, "/test_id", nil)
-	w := httptest.NewRecorder()
+// 	// Создание HTTP-запроса
+// 	request := httptest.NewRequest(http.MethodGet, "/test_id", nil)
+// 	w := httptest.NewRecorder()
 
-	// Выполнение запроса
-	r.ServeHTTP(w, request)
+// 	// Выполнение запроса
+// 	r.ServeHTTP(w, request)
 
-	// Выводим результат
-	fmt.Println(w.Code)
-	fmt.Println(w.Header().Get("Location"))
+// 	// Выводим результат
+// 	fmt.Println(w.Code)
+// 	fmt.Println(w.Header().Get("Location"))
 
-	// Output:
-	// 307
-	// https://practicum.yandex.ru/
-}
+// 	// Output:
+// 	// 307
+// 	// https://practicum.yandex.ru/
+// }
